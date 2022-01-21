b79078518bf4f22d16cd33d3f76bcf6644e31cef
Abdullah Baig <rebelschaos@gmail.com>
2022-01-21 13:52:29 +0500

    rollback with hex

---
+++ src/goerliBot.js
@@ -0,0 +1,125 @@
+require('dotenv').config({path: '../.env'})
+
+const utils = require('./utils.js');
+const Discord = require('discord.js');
+const etherscan = require('./api.js');
+const db = require('./db.js');
+const Web3 = require('web3');
+const { max } = require('pg/lib/defaults');
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.INFURA_HTTPS_ENDPOINT));
+
+const DEFAULT_GAS_PRICE = 1500000000000; // 1,500 gwei
+
+const INELIGIBLE_NO_CUSTOM_CHECKS_MESSAGE = " is ineligible to receive goerli eth.";
+const INELIGIBLE_CUSTOM_CHECKS_MESSAGE = " is ineligible to receive goerli eth.  You must pass the custom checks;";
+
+const maxDepositAmount = Number(process.env.MAX_DEPOSIT_AMOUNT) 
+
+const runCustomEligibilityChecks = async (address, topUpAmount) => {
+  const res = await db.confirmTransaction(address, topUpAmount/Math.pow(10,18));
+  console.log(res)
+  return res
+
+}
+
+const receiverIsEligible = async (address, amountRequested, runCustomChecks)  => {
+  const needsGoerliEth = true;
+  if (runCustomChecks) {
+    const passedCustomChecks = await runCustomEligibilityChecks(address, amountRequested);
+    return needsGoerliEth && passedCustomChecks;
+  } else {
+    return needsGoerliEth;
+  }
+}
+
+const runGoerliFaucet = async (message, address, runCustomChecks) => {
+  let embed = new Discord.MessageEmbed();
+  const currentBalance = await etherscan.getBalance(address);
+  if (currentBalance === null) {
+    console.log("Something went wrong while connecting to API to recieve balance.");
+    if (message) {
+      embed.setDescription("**Error**\nSomething went wrong while getting address details please try again.").
+      setTimestamp().setColor(0xff1100);
+      message.lineReply(embed)
+    }
+    return;
+  }
+
+  const topUpAmount = maxDepositAmount - (currentBalance);
+
+  if(topUpAmount <= 0 ) {
+    console.log("Address has max deposit amount.");
+
+    if (message) {
+      embed.setDescription("**Operation Unsuccessful**\nAddress has max deposit amount.").
+      setTimestamp().setColor(0xff1100);
+      message.lineReply(embed);
+    }
+    return;
+  }
+
+  console.log("address " + address + " is requesting " + topUpAmount/Math.pow(10,18) + " goerli eth.  Custom checks: " + runCustomChecks);
+
+  // Make sure the bot has enough Goerli ETH to send
+  const faucetReady = await utils.faucetIsReady(process.env.FAUCET_ADDRESS, (topUpAmount + 1500000000000)/Math.pow(10,18));
+  if (!faucetReady) {
+    console.log("Faucet does not have enough ETH.");
+
+    if (message) {
+      embed.setDescription("**Operation Unsuccessful**\nThe Bot does not have enough Goerli ETH.  Please contact the maintainers.").
+      setTimestamp().setColor(0xff1100);
+      message.lineReply(embed);
+    }
+    return;
+  }
+
+  const receiverEligible = await receiverIsEligible(address, topUpAmount, runCustomChecks);
+  if (receiverIsEligible === null){
+    if (message) {
+      embed.setDescription('**Error**\nSomething went wrong while confirming your transaction please try again.')
+          .setTimestamp().setColor(3447003);
+      message.lineReply(embed);
+    }
+  }
+
+  if (!receiverEligible) {
+    const m = runCustomChecks ? address + INELIGIBLE_CUSTOM_CHECKS_MESSAGE
+      : address + INELIGIBLE_NO_CUSTOM_CHECKS_MESSAGE;
+
+    console.log(m);
+
+    if (message) {
+      embed.setDescription(m)
+          .setTimestamp().setColor(3447003);
+      message.lineReply(embed);
+    }
+    return;
+  }
+
+  console.log("Checks passed - sending to " +  address);
+  if (message) {
+    embed.MessageEmbed().setDescription("**Operation Successful**\nChecks passed - sending...").
+    setTimestamp().setColor(3447003);
+    message.lineReply(embed);
+  }
+
+  const nonce = utils.getCachedNonce();
+  utils.sendGoerliEth(message, process.env.FAUCET_ADDRESS, process.env.FAUCET_PRIVATE_KEY, address, topUpAmount/Math.pow(10,18), nonce, DEFAULT_GAS_PRICE);
+  
+  utils.incrementCachedNonce();
+}
+
+// This runs once when imported (bot starting) to cache the nonce in a local file
+utils.initializeCachedNonce();
+
+module.exports = {
+  name: 'goerliBot',
+  description: 'Sends goerli eth to the user.',
+  execute(message, args, runCustomChecks = true) {
+    runGoerliFaucet(message, args[1], runCustomChecks);
+  }
+} 
+
+utils.initializeCachedNonce();
+
+//runGoerliFaucet(null, "0x066Adead2d82A1C2700b4B48ee82ec952b6b18dA", true);
---
+++ src/main.js
@@ -0,0 +1,104 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const goerliBot = require('./goerliBot.js');
+require('discord-reply');
+const bot = new Discord.Client();
+const web3 = require('web3');
+
+const COMMAND_PREFIX = '+goerlieth';
+const EMBEDDED_HELP_MESSAGE = {
+  embed: {
+    color: 3447003,
+    title: "Goerli ETH Bot",
+    description: "Welcome to the Goerli ETH Faucet.  See below for my commands.",
+    fields: [{
+        name: "+goerlieth <hex-data>",
+        value: "`Sends up to 32 goerli ETH to the specified Hex. \n\nEx: +goerlieth <hex-data>`"
+      },
+      {
+        name: "+goerlieth help",
+        value: "`Shows this message.`"
+      },
+      {
+        name: "+goerlieth mod",
+        value: "`Tags the maintainers of this bot, please use if you are experiencing any issues.`"
+      }
+    ]
+  }
+}
+
+let yourBotToken = process.env.DISCORD_BOT_TOKEN;
+bot.login(yourBotToken);
+
+bot.commands = new Discord.Collection();
+bot.commands.set(goerliBot.name, goerliBot);
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
+function prefix0x(hex){
+  if (!hex.startsWith('0x')){
+    return `0x${hex}`
+  }else{
+    return hex
+  }
+}
+
+bot.on('message', (message) => {
+  try {
+    if (!message || message.length === 0 || message.content.substring(0, COMMAND_PREFIX.length) !== COMMAND_PREFIX) {
+      return;
+    }
+
+    let embed = new Discord.MessageEmbed()
+
+    const args = message.content.substring(COMMAND_PREFIX.length).split(" ")
+
+    if (web3.utils.isHexStrict(prefix0x(args[1]))){
+      bot.commands.get('goerliBot').execute(message, args, true);
+    }else{
+      embed.setDescription('**Error**\nInvalid `Hex`. Please double check.')
+          .setColor(0xff1100).setTimestamp();
+      message.lineReply(embed);
+    }
+    
+    switch(args[1]){ 
+      // Faucet commands
+      case 'null': {
+        embed.setDescription('**Error**\nUse `+goerlieth help` for the list of commands!')
+            .setColor(0xff1100).setTimestamp();
+        message.lineReply(embed);
+        break;
+      }
+      // Other commands
+      case 'help': {
+        console.log("help called");
+        message.lineReply(EMBEDDED_HELP_MESSAGE);
+        break;
+      }
+      case 'mod': {
+        // Tag the moderators
+        console.log("mod called");
+        // Uncomment below and add discord ids if you'd like to be tagged
+        embed.setDescription('**Alerting the Administrators**\n <@&723840404159594496> come check this out!')
+            .setColor(3447003).setTimestamp();
+        message.lineReply(embed);
+        break;
+      }
+      // For fun :)
+      case 'dance': {
+        console.log("dance called");
+        embed.setImage('https://c.tenor.com/fJh-W38iA3oAAAAM/dance-kid.gif').setColor(3447003).setTimestamp();
+        message.lineReply(embed);
+        break;
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    let embed = new Discord.MessageEmbed().setDescription('**Error**\nSomething went wrong. If this continues,' +
+        ' please contact the mods of this bot by using command: `!mod`').setColor(0xff1100).setTimestamp();
+    message.lineReply(embed);
+  }
+});
