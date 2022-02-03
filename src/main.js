7344810054e4aef073ffe48f016264897db8c3f7
guy muroch <guym@blox.io>
2022-02-03 18:43:32 +0200

    change function

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ let channelIsOnline = true;
 const textColor = 0xff1100;
 const COMMAND_PREFIX = '+goerlieth';
 const title = 'SSV Goerli Deposit Bot';
-const adminID = [844110609142513675, 724238721028980756, 876421771400740874];
+const adminID = [844110609142513675,  724238721028980756, 876421771400740874];
 
 const EMBEDDED_HELP_MESSAGE = new Discord.MessageEmbed().setTitle(title).setColor(3447003)
     .setDescription(config.MESSAGES.MODE.HELP)
@@ -47,6 +47,8 @@ bot.on('message', async (message) => {
             console.log('<<<<<<<<<<<close channel>>>>>>>>>>>')
             channelIsOnline = false;
             await channel.updateOverwrite(config.VERIFIED_ROLE_ID, {SEND_MESSAGES: false});
+            embed.setDescription(config.MESSAGES.ERRORS.END_OF_CYCLE).setTimestamp().setColor(0xff1100);
+            await message.lineReply(embed);
             return;
         }
 
