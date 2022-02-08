dc8396f008938abb51eef65ed7039464043b7b1d
guy muroch <guym@blox.io>
2022-02-08 17:54:01 +0200

    merge stage

--- src/queueHandler.js
+++ src/queueHandler.js
@@ -23,8 +23,8 @@ class QueueHandler {
         })
 
         for (let key in itemsKeys) {
-            console.log(`Running ' + ${(Number(key) + 1)} + '/' + ${itemsKeys.length} `,item.address);
             const item = JSON.parse(await this.redisStore.client.get(itemsKeys[key]));
+            console.log(`Running ' + ${(Number(key) + 1)} + '/' + ${itemsKeys.length} `,item.address);
             if(item.formSubmitted) {
                 await goerliBot.runGoerliFaucet(item.message, item.address, item.hexData);
                 await this.redisStore.removeFromQueue(itemsKeys[key]);
